45e66dfea8e2dfaee98c38055a0d32415a904efe
Philip Miesbauer <philip.miesbauer@gmail.com>
2023-08-05 10:32:15 +0100

    Initial definition of steps
    This commit just records the initial steps, that I currently think are
    required to get the simulation to run.

---
+++ simulate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+set -euo pipefail
+
+: ' A script to simulate a group of individuals,
+how diverse they are and based on that diversity,
+how likely they are to find the ideal solution to a problem.
+
+Written by Philip Miesbauer
+'
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+
---
+++ src/simulate.py
@@ -0,0 +1,11 @@
+
+
+# 1. Calc probabilities attributes occuring
+
+# 2. Distribute attributes to individuals
+
+# 3. Select teams of certain size randomly from population
+
+# 4. Calculate diversity score
+
+# 5. Calculate probability of solving a problem with certain requirements
