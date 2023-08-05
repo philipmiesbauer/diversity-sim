5928fff764fb38fc73a81061c90b7f8c014cbf48
Philip Miesbauer <philip.miesbauer@gmail.com>
2023-08-05 11:17:53 +0100

    Generate population
    This commit adds the code to generate a population of indiviuals of a
    with an individual set of attributes distrubted according to the
    individual attributes probability of occuring

---
+++ src/classes.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import random
+from wonderwords import RandomWord
+import names
+
+randomWord = RandomWord()
+
+class Attribute:
+    """Calls representing an attribute"""
+
+    def __init__(self) -> None:
+        self.name = randomWord.word()
+        self.probability = random.random()
+        self.count = 0  # Indicates how often this attributes is actually found in the population
+
+    def __str__(self) -> str:
+        return self.name + "(" + str(self.probability) + "/" + str(self.count) + ")"
+
+    def __eq__(self, __value: object) -> bool:
+        return (
+            self.name == __value.name and
+            self.probability == __value.probability
+        )
+
+class Individual:
+    """Class representing an individual"""
+
+    def __init__(self) -> None:
+        self.name = names.get_full_name()
+        self.attributes = []
+
+    def add_attribute(self, new_attribute: Attribute) -> None:
+        self.attributes.append(new_attribute)
+        new_attribute.count = new_attribute.count + 1
+
+    def __str__(self) -> str:
+        return_str = self.name + ": "
+        for attribute in self.attributes:
+            return_str = return_str + str(attribute) + ", "
+        if len(self.attributes) > 0:
+            return_str = return_str[:-2]
+        return return_str
+
+    def __eq__(self, __value: object) -> bool:
+        return (
+            self.name == __value.name and
+            self.attributes == __value.attributes
+        )
--- src/simulate.py
+++ src/simulate.py
@@ -1,8 +1,27 @@
+#!/usr/bin/python
+import random
+from classes import Individual
+from classes import Attribute
 
+# random.seed(0)
+NUM_ATTRIBUTES = 10
+POPULATION_SIZE = 1000
 
-# 1. Calc probabilities attributes occuring
+# 1. Generate attributes
+attributes = [Attribute() for i in range(NUM_ATTRIBUTES)]
+# for attribute in attributes:
+#     print(attribute.name + ": " + str(attribute.probability))
 
 # 2. Distribute attributes to individuals
+population = [Individual() for i in range(POPULATION_SIZE)]
+for individual in population:
+  for attribute in attributes:
+      # Determine if they have the attribute
+      if random.random() < attribute.probability:
+         individual.add_attribute(attribute)
+
+for attribute in attributes:
+   print(str(attribute))
 
 # 3. Select teams of certain size randomly from population
 
